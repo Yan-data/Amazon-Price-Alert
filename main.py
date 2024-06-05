8ab237870980ee90ea1549256c9d889e0073601a
Yan Cheng <yan.chneg@rub.de>
2024-06-05 13:54:38 +0200

    add selenium for title scraping

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+#import csv
 import time
 from datetime import datetime
 import requests
@@ -7,10 +7,11 @@ import lxml
 import smtplib
 import os
 import psycopg2
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-ALERT_PRICE = 90
+ALERT_PRICE = 500
 URL = os.environ.get('URL')
-print(URL)
 USER_AGENT = os.environ.get('USER_AGENT')
 ACCEPT_LANGUAGE = os.environ.get('ACCEPT_LANGUAGE')
 SMTP_ADDRESS = os.environ.get('SMTP_ADDRESS')
@@ -19,39 +20,44 @@ PASSWORD = os.environ.get('PASSWORD')
 headers = {
     'Accept-Language': ACCEPT_LANGUAGE,
     'User-Agent': USER_AGENT,
-    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"'
+    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
 }
 
-# create table in postgres
-conn = psycopg2.connect(database="Amazon_prices",
-                        host="db_host",
-                        user="db_user",
-                        password="db_pass",
-                        port=5432)
-# Open a cursor to perform database operations
-cur = conn.cursor()
-# Execute a command: create a price table
-cur.execute("""CREATE TABLE prices(
-            price_id SERIAL PRIMARY KEY,
-            item_title VARCHAR (50) NOT NULL,
-            price FLOAT NOT NULL,
-            time DATETIME UNIQUE NOT NULL);
-            """)
+# # create table in postgres
+# conn = psycopg2.connect(database="Amazon_prices",
+#                         host="db_host",
+#                         user="db_user",
+#                         password="db_pass",
+#                         port=5432)
+# # Open a cursor to perform database operations
+# cur = conn.cursor()
+# # Execute a command: create a price table
+# cur.execute("""CREATE TABLE prices(
+#             price_id SERIAL PRIMARY KEY,
+#             item_title VARCHAR (50) NOT NULL,
+#             price FLOAT NOT NULL,
+#             time DATETIME UNIQUE NOT NULL);
+#             """)
 
-
+driver = webdriver.Chrome()
+driver.get(URL)
 def check_price():
     # get price data
     response = requests.get(url=URL, headers=headers)
+    #print(response.text)
     soup = BeautifulSoup(response.text, 'lxml')
     price_tag = soup.find(class_="aok-offscreen")
-    price = float(price_tag.getText().split('$')[1])
-    title = soup.find(id="productTitle").get_text().strip()
+    price_str = price_tag.getText().split()[0]
+    price = float(price_str.split('€')[1])
+    #title = soup.find(class_='product-title-word-break').getText()
+    title = driver.find_element(by=By.ID, value='productTitle').text
+    driver.close()
     today = datetime.now().date()
     # data = [title, price, today]
-    # save data to csv
+    # # save data to csv
     # with open(f'amazon_price_list', 'a+', newline='', encoding='UTF8') as file:
-    #   writer = csv.writer(file)
-    #  writer.writerow(data)
+    #     writer = csv.writer(file)
+    #     writer.writerow(data)
     # insert data to db
     cur.execute("INSERT INTO prices(item_title, price, time) VALUES(title,price,today)")
     # Make the changes to the database persistent
